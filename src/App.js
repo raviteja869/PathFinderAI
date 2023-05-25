a1343820f615b7a8b39e6a4ee503f9a3e9f9e78f
raviteja869 <132315611+raviteja869@users.noreply.github.com>
2023-05-25 06:06:28 +0000

    Changes to be committed: modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Header from './components/Header';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      {/* other components */}
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <h1>Hello, this is a test message!</h1>
+        {/* other components */}
+      </div>
+    </Router>
   );
 }
 
